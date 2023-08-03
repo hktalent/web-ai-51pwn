62da4b0adbaa2e66ec2ed640e496571927348e45
hktalent <18223385+hktalent@users.noreply.github.com>
2023-08-03 18:46:19 +0800

    优化静态资源从cdnjs unpkg，加载 2023-08-03

--- ps.sh
+++ ps.sh
@@ -1,3 +1,12 @@
 yarn build
+
+# for ii in {0..11}  
+# do  
+#     wget --no-check-certificate -c -P dist/ https://127.0.0.1:2083/js/${ii}.js
+# done
+wget --no-check-certificate -c -P dist/ https://127.0.0.1:2083/js/8.js
+#  https://www.yiibai.com/bash/bash-for-loop.html
+
+# https://unpkg.com/web-ai-51pwn@latest/dist/index.bundle.js
 npm publish
 
